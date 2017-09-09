46e93d8e016a57e19fae7a958433a52869fa4dd6
Simbidion <simonadwyer@yahoo.com.au>
2017-09-09 22:09:48 +1000

    drge

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,4 +6,10 @@ $(document).scroll(function(e) {
   $(window).scrollTop() > transition ? $('#small-head').addClass('show') : $('#small-head').removeClass('show');
 });
 
-
+$(document).scroll(function(e) {
+  height = $( window ).height();
+  transition = 100;
+  $(window).scrollTop() > transition ? $('#big-header').addClass('trans-big-header') : $('#big-header').removeClass('trans-big-header');
+  $(window).scrollTop() > transition ? $('#h1-container').addClass('trans-h1-container') : $('#h1-container').removeClass('trans-h1-container');
+  $(window).scrollTop() > transition ? $('#h1').addClass('trans-h1') : $('#h1').removeClass('trans-h1');
+});
